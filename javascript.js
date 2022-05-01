f6dc531155edd3f91b2b8d77b9acd9b29bfef5a6
yaakovbockian <yaakovbockian@gmail.com>
2022-05-01 12:12:05 -0400

    javascript and gitignore

---
+++ javascript.js
@@ -0,0 +1,26 @@
+document.getElementById("button1").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "450px";
+    document.getElementById("box").style.width = "450px";
+
+});
+
+document.getElementById("button2").addEventListener("click", function(){
+
+document.getElementById("box").style.background = "blue";
+
+});
+
+document.getElementById("button3").addEventListener("click", function(){
+
+    document.getElementById("box").style.opacity = ".6";
+
+});
+
+document.getElementById("button4").addEventListener("click", function(){
+
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.width = "150px";
+    document.getElementById("box").style.opacity = "1";
+    document.getElementById("box").style.background = "orange";
+});
